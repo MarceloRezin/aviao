9a8ce890725b5add7ae112721933fe6844a4eccf
Marcelo Rezin <marcelo.rezin@gmail.com>
2019-06-08 18:21:47 -0300

    Exemplo de inserção de aviões

--- app/index-controller.js
+++ app/index-controller.js
@@ -5,6 +5,23 @@ var aviaoApp = angular.module('AviaoApp', ['ngMaterial', 'ngMessages'])
     .primaryPalette('indigo');
 })
 
-.controller('IndexCtrl', function($scope, Console) {
-    Console.push('Radar iniciado')
+.controller('IndexCtrl', function($scope, Console, AviaoService, Aviao) {
+    Console.push('Radar iniciado');
+
+    var a = new Aviao();
+    a.setX(10);
+    a.setY(10);
+
+    var b = new Aviao();
+    b.setX(14);
+    b.setY(14);
+    b.inverterSelecao();
+
+    var c = new Aviao();
+    c.setX(19);
+    c.setY(14);
+
+    AviaoService.addAviao(a);
+    AviaoService.addAviao(b);
+    AviaoService.addAviao(c);
 });
