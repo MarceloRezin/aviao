862fa921fece6eecc69419b5de3909895c1f7ddf
Lucas <lucasclaro10@hotmail.com>
2019-06-09 01:07:21 -0300

    Inserindo Campos da "Entrada"
    Inserindo Campos da "Entrada"

--- app/src/entrada/entrada-controller.js
+++ app/src/entrada/entrada-controller.js
@@ -1,3 +1,11 @@
-aviaoApp.controller('EntradaCtrl', function($scope) {
+aviaoApp.controller('EntradaCtrl', function($scope, AviaoService, Aviao) {
+    $scope.cb1 = false
+
+    $scope.addAv = function() {
+        var aviao = new Aviao();
+        aviao.setX($scope.x);
+        aviao.setY($scope.y);
+        AviaoService.addAviao(aviao);
+    };
 
 });
