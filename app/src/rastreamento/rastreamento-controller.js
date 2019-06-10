d6d948c98f1c43350579fd38a333345729999e43
Claro <Lucas.Claro@thomsonreuters.com>
2019-06-10 15:57:29 -0300

    Complemento do arquivo "Entradas" e criação dos campos "Rastreamento"
    Complemento do arquivo "Entradas" e criação dos campos "Rastreamento"

--- app/src/rastreamento/rastreamento-controller.js
+++ app/src/rastreamento/rastreamento-controller.js
@@ -1,4 +1,32 @@
 aviaoApp.controller('RastreamentoCtrl', function($scope) {
 
-    $scope.cde = '345';
+    $scope.verificarDistAero = function() {
+        if($scope.distanciaAeroporto == null || $scope.distanciaAeroporto === "undefined"){
+            $scope.distanciaAeroportoErro = true;
+        }else{
+            alert("Distancia Minima da Base: " + $scope.distanciaAeroporto)
+        }
+    }
+
+    $scope.verificarDistAv = function() {
+        if($scope.distanciaAvioes == null || $scope.distanciaAvioes === "undefined"){
+            $scope.distanciaAvioesErro = true;
+        }else{
+            alert("Distancia Minima Entre Aviões: " + $scope.distanciaAvioes)
+        }
+    }
+
+    $scope.verificarTempoColisao = function() {
+        if($scope.tempoColisao == null || $scope.tempoColisao === "undefined"){
+            $scope.tempoColisaoErro = true;
+        }else{
+            alert("Tempo Minimo para Colisão: " + $scope.tempoColisao)
+        }
+    }
+
+    $scope.zeraCampos = function() {
+        $scope.distanciaAeroportoErro = false;
+        $scope.distanciaAvioesErro = false;
+        $scope.tempoColisaoErro = false;
+    }
 });
