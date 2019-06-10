934e0ea22918b30dcb87219f0241be15a11e1344
Claro <Lucas.Claro@thomsonreuters.com>
2019-06-10 14:32:10 -0300

    Validações dos Campos
    Validações dos Campos

--- app/src/entrada/entrada-controller.js
+++ app/src/entrada/entrada-controller.js
@@ -1,10 +1,20 @@
 aviaoApp.controller('EntradaCtrl', function($scope, AviaoService, Aviao) {
-    $scope.cb1 = false
+    $scope.cb1 = false;
 
     $scope.addAv = function() {
         var aviao = new Aviao();
-        aviao.setX($scope.x);
-        aviao.setY($scope.y);
+        if($scope.cb1 == true){
+            aviao.setRaio($scope.raio);
+            aviao.setAngulo($scope.angulo);
+            aviao.setVelocidade($scope.velocidade);
+            aviao.setDirecao($scope.direcao);
+        }
+        else{
+            aviao.setX($scope.x);
+            aviao.setY($scope.y);
+            aviao.setVelocidade($scope.velocidade);
+            aviao.setDirecao($scope.direcao);
+        }
         AviaoService.addAviao(aviao);
     };
 
